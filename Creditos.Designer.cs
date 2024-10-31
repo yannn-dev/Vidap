cf9cc95727d2a3e7288e851269e662cb21d4e1cc
Kaique <yannickaique@outlook.com>
2024-10-31 08:20:16 -0300

    Alterações pequenas de layout e correção ortográfica

--- Creditos.Designer.cs
+++ Creditos.Designer.cs
@@ -167,6 +167,7 @@
             this.Controls.Add(this.lblCreditosOficial);
             this.Controls.Add(this.panelCabecalho);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Creditos";
